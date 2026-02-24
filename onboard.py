a5b6353b42ccd1c4aa1785a2715328274211e318
Venkatram S <venkatram.csc1@gmail.com>
2026-02-24 23:51:25 +0530

    Update onboard.py

--- onboard.py
+++ onboard.py
@@ -20,7 +20,7 @@ def config_file_creator(mode):
 	data["brave_api_key"]=brave_api_key
 	max_tokens=inputInt("Enter Max Tokens [Click Enter for 8192]: ",default = 8192,blank=True)	
 	data["max_tokens"] = 8192 if max_tokens == "" else max_tokens
-	temperature= inputNum("Enter Max Tokens [Click Enter for 8192]: ",default = 0.7,blank=True)
+	temperature= inputNum("Enter Temperature [Click Enter for 0.7]: ",default = 0.7,blank=True)
 	data["temperature"] = 0.7 if temperature == "" else temperature
 	agent_name=inputStr("Enter Agent Name [Click Enter to leave it blank]: ",blank=True,default="PyClaw")
 	data["agent_name"]=agent_name
@@ -45,3 +45,4 @@ def load_config():
 	data=None
 	with open(filepath_str, "r") as f:
 		data=load(f)
+	return data
