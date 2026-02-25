da75d5e0530c4ae1965af75ea1c7cae54f1fa891
Venkatram S <venkatram.csc1@gmail.com>
2026-02-25 16:01:54 +0530

    Create __main__.py

---
+++ __main__.py
@@ -0,0 +1,29 @@
+from argparse import ArgumentParser
+from src.onboard import *
+from sys import version
+python_ver=version[0:4]
+
+#Inspired from PicoCLaw (https://github.com/sipeed/picoclaw/)
+help_str="""Usage: pyclaw <command>\nCommands:\n- onboard     Initialize pyclaw configuration\n- query     single question, single answer, exits immediately\n- agent     single question, single answer, exits immediately\n- cron        Manage scheduled tasks\n- version     Show version information\n- help     Show help information"""
+pyclaw_ver="0.0.1"
+
+def main():
+	parser = ArgumentParser(prog="pyclaw",description="PyClaw - A Python-based AI Assistant")
+	parser.add_argument('command',type=str)
+	parser.set_defaults(foo=help_str,verbose=False)
+	args = parser.parse_args()
+	if (args.command==None):
+		print(help_str)
+	if (args.command=="onboard"):
+		check_onboard_already_exists()
+	if (args.command=="help"):
+		print(help_str)
+	if (args.command=="version"):
+		print(f"Pyclaw {pyclaw_ver} runs on Python {python_ver}")
+	if (args.command=="agent"):
+		pass
+	if (args.command=="query"):
+		pass
+
+if __name__=="__main__":
+	main()
