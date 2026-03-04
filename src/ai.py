4ef17d594447eefe9513b7f5b26c366e7b5f652f
Venkatram S <venkatram.csc1@gmail.com>
2026-03-04 23:20:33 +0530

    Implemented tone functionality to improve responses

--- src/ai.py
+++ src/ai.py
@@ -2,9 +2,10 @@ from groq import Groq
 from src.onboard import filepath_str
 from pathlib import Path
 from os import path
-from json import load
+from json import load,dumps
 from re import sub
-#client = Groq(api_key=)
+from requests import request
+
 def load_config():
   data=dict()
   if (path.exists(filepath_str)):
@@ -18,8 +19,8 @@ def load_config():
 def strip_md(text):
   #text=sub(r'(\*\*|__)(.*?)\1', r'\2',text)
   agent_name=load_config()['agent_name']
-  text=text.replace('<think>',agent_name+'thinking:')
-  text=text.replace('</think>',agent_name+'thinking ended.')
+  text=text.replace('<think>',agent_name+'thinking: \n')
+  text=text.replace('</think>',agent_name+'thinking ended.\n')
   text=text.replace('#','')
   text=text.replace('##','')
   text=text.replace('###','')
@@ -33,8 +34,8 @@ def strip_md(text):
 def return_api(api_type):
   if api_type=="groq":
     return os.getenv("groq_api_key")
-  elif api_type=="brave":
-    return os.getenv("groq_api_key")"""
+  elif api_type=="langsearch":
+    return os.getenv("langsearch_api_key")"""
 
 def chat(message):
   config=load_config()
@@ -50,3 +51,17 @@ def chat(message):
     model=config['model_name'])
   content = chat_completion.choices[0].message.content
   return strip_md(content)
+
+def langsearch(query):
+  langsearch_api=load_config()["langsearch_api_key"]
+  payload = json.dumps({
+  "query": query,
+  "freshness": "noLimit",
+  "summary": True,
+  "count": 10})
+  headers = {
+  'Authorization': langsearch_api,
+  'Content-Type': 'application/json'
+  }
+  response = requests.request("POST", url, headers=headers, data=payload)
+  return response.text
